228459f1291e94fdf8a03f0c60959a47ec0573a1
lavanyapareek <lavanyapareek@gmail.com>
2025-02-04 21:47:44 +0530

    Question 1 PS4 added -_-

---
+++ ProblemSet/PS4/Problem1.py
@@ -0,0 +1,72 @@
+'''
+Consider the system of linear equations of the form Ax= b. Given the
+matrix A (of size mxn) and the vector b (of length n), write a code that
+finds whether the system has a unique solution, infinite solutions, or no
+solutions. Furthermore, the code must compute the solution if it is unique,
+characterize the complete solution if there are infinitely many, and provide
+the best fit solution along with the squared error if there are no solutions.
+'''
+import numpy as np
+import numpy.linalg as la
+
+A = np.array([[1, 2, 3, 4, 5], [4, 17, 6, 31, 47], [11, 8, 9, 19, 23], [10, 13, 12, 29, 37]])
+b = np.array([1, 2, 3, 4])
+
+def linear_system(A, b):
+    m, n = A.shape
+    if b.shape[0] != m:
+        print("Mismatch : Number of rows in A must match length of b.")
+        return None
+    
+    r = la.matrix_rank(A)
+    r_Ab = la.matrix_rank(np.column_stack((A, b)))
+
+    if r == r_Ab:
+        #Solutions will exist only if the augmented matrix A|b is a full rank matrix
+        if r == n:
+            #Unique Solutions
+            print("System of Equations has a unique solution")
+            x, residuals, rank, s = la.lstsq(A, b, rcond = None)
+            print("Squared Error : ", np.sum(residuals) if residuals.size > 0 else 0.0)
+            print(x)
+        elif r != n:
+            #Infinite Solutions
+            print("System has infinite solutions")
+            x, residuals, rank, s = la.lstsq(A, b, rcond = None)
+            print("Least-squared solution : \n", x)
+            print("Squared Error : ", np.sum(residuals) if residuals.size > 0 else 0.0)
+            U, S, Vt = la.svd(A)
+            print("Basis for null space of A : \n", Vt.T[:, r:])
+    else:
+        #No Solutions
+        print("System Has no Solutions")
+        x, residuals, rank, s = la.lstsq(A, b, rcond = None)
+        print("Least-Squared solution : \n", x.T)
+        print("Squared Error : ", np.sum(residuals) if residuals.size > 0 else 0.0)
+    return "Solved!"
+
+'''
+Note :
+x, residuals, rank, s = la.lstsq(A, b, rcond = None)
+
+gives, 
+    x : closest possible solution
+    residuals : b - a @ x
+    rank : Rank of A
+    s : Singular values of A
+
+    From Documentation :
+
+    Computes the vector x that approximately solves the equation a @ x = b. 
+    The equation may be under-, well-, or over-determined 
+    (i.e., the number of linearly independent rows of a can be less than, equal to, 
+    or greater than its number of linearly independent columns). 
+    If a is square and of full rank, then x (but for round-off error) is the “exact” 
+    solution of the equation. Else, x minimizes the Euclidean 2-norm. 
+    If there are multiple minimizing solutions, the one with the smallest 2-norm 
+
+ is returned.
+'''
+
+print(linear_system(A, b))
+    
---
+++ ProblemSet/PS4/Problem3.py
@@ -0,0 +1,7 @@
+'''
+Assume that you are given a matrix A = [a1, a2, . . . , am] where ai ∈Rn are
+n-length vectors. Consider m < n, and that A is an orthonormal matrix.
+Write a code to construct another (n-m) vectors am+1, am+2, . . . , an such
+that the matrix B = [a1, . . . , an] is still an orthonormal matrix.
+'''
+
